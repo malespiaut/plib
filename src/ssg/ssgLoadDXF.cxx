6e5bc8b68f711091da5076f8e728f21d0aba9485
mcdave <mcdave@713b2cfd-b11f-0410-b270-d5401603882f>
2000-03-29 03:04:45 +0000

    fixed dxf polyline support

--- src/ssg/ssgLoadDXF.cxx
+++ src/ssg/ssgLoadDXF.cxx
@@ -10,79 +10,146 @@
 #define MAX_LINE_LEN 1024
 #define MAX_VERT 100000
 
-enum Modes { MODE_NONE, MODE_LINE, MODE_FACE, MODE_POLYLINE };
+enum Modes { MODE_NONE, MODE_LINE, MODE_FACE, MODE_POLYLINE, MODE_VERTEX };
 
 static ssgHookFunc      current_hookFunc = NULL ;
 static ssgBranch       *current_branch   = NULL ;
 
-static int   num_line;
-static int   num_face;
-static int   num_linevert;
-static int   num_facevert;
+static int num_line;
+static int num_linevert;
 static sgVec3* linevert;
+
+static int num_face;
+static int num_facevert;
 static sgVec3* facevert;
 
-static int   mode;
-static int   num_vert;
+static int num_meshvert;
+static sgVec3* meshvert;
+static int meshface[4];
+static int meshflag;
 
+static int mode;
+static int num_vert;
+static int cflags;
+static sgVec3 cvec;
 
 static void dxf_flush ( void )
 {
-  if ( num_vert > 0 ) {
-    if ( mode == MODE_LINE ) {
-      if ( num_vert >= 2 ) {
-        num_line ++;
-        num_linevert += 2;
-      }
-      num_vert = 0;
-    }
-    else if ( mode == MODE_FACE ) {
-      if ( num_vert >= 3 ) {
-        //quad?
-        if ( num_vert >= 4 && num_facevert + 6 < MAX_VERT ) {
-          sgCopyVec3( facevert[num_facevert + 4], facevert[num_facevert + 1] );
-          sgCopyVec3( facevert[num_facevert + 5], facevert[num_facevert + 2] );
-          sgCopyVec3( facevert[num_facevert + 2], facevert[num_facevert + 3] );
-          num_face += 2;
-          num_facevert += 6;
-        }
-        else {
-          //triangle
-          num_face += 1;
-          num_facevert += 3;
-        }
-      }
-      num_vert = 0;
+  if ( mode == MODE_LINE ) {
+    if ( num_vert >= 2 ) {
+      num_line ++;
+      num_linevert += 2;
     }
   }
+  else if ( mode == MODE_FACE ) {
+    if ( num_vert >= 3 ) {
+      //quad?
+      if ( num_vert >= 4 && num_facevert + 6 < MAX_VERT ) {
+        sgCopyVec3( facevert[num_facevert + 4], facevert[num_facevert + 1] );
+        sgCopyVec3( facevert[num_facevert + 5], facevert[num_facevert + 2] );
+        sgCopyVec3( facevert[num_facevert + 2], facevert[num_facevert + 3] );
+        num_face += 2;
+        num_facevert += 6;
+      }
+      else {
+        //triangle
+        num_face += 1;
+        num_facevert += 3;
+      }
+    }
+  }
+  else if ( mode == MODE_POLYLINE ) {
+    if ( (cflags & 64) != 0 )
+       meshflag = 1;
+    else
+       meshflag = 0;
+    num_meshvert = 0;
+  }
+  else if ( mode == MODE_VERTEX ) {
+    //does this vertex belong to a polyface mesh?
+    if ( meshflag && (cflags & 128) != 0 ) {
+      //is this a vertex?
+      if ( (cflags & 64) != 0 ) {
+        if ( num_meshvert < MAX_VERT ) {
+          sgCopyVec3( meshvert[num_meshvert], cvec );
+          num_meshvert ++ ;
+        }
+      }
+      else if ( num_vert >= 3 && num_facevert + num_vert < MAX_VERT ) {
+
+        //copy each vertex where the first is numbered 1
+        int error = 0;
+        for ( int i=0; i<num_vert; i++ )
+        {
+          int ival = meshface[i];
+          if ( ival < 0 )
+            ival = -ival;  //invisible vertex whatever that means :)
+          if ( ival > 0 && ival <= num_meshvert )
+            sgCopyVec3( facevert[num_facevert + i], meshvert[ival-1] );
+          else
+            error = 1;
+        }
+
+        if ( error == 0 ) {
+          //quad?
+          if ( num_vert >= 4 && num_facevert + 6 < MAX_VERT ) {
+            sgCopyVec3( facevert[num_facevert + 4], facevert[num_facevert + 1] );
+            sgCopyVec3( facevert[num_facevert + 5], facevert[num_facevert + 2] );
+            sgCopyVec3( facevert[num_facevert + 2], facevert[num_facevert + 3] );
+            num_face += 2;
+            num_facevert += 6;
+          }
+          else {
+            //triangle
+            num_face += 1;
+            num_facevert += 3;
+          }
+        }
+      }
+    }
+  }
+
+  num_vert = 0;
+  cflags = 0;
+  cvec[0] = 0.0f;
+  cvec[1] = 0.0f;
+  cvec[2] = 0.0f;
+  meshface[0] = 0;
+  meshface[1] = 0;
+  meshface[2] = 0;
+  meshface[3] = 0;
 }
 
 
 static int dxf_read ( FILE *filein )
 {
-  int   code;
-  int   count;
-  char  input1[MAX_LINE_LEN];
-  char  input2[MAX_LINE_LEN];
-  float rval;
-  int   width;
-  int   cpos;
-  sgVec3 cvec;
+/*
+initialize lists
+*/
+  num_line = 0;
+  num_linevert = 0;
+  linevert = new sgVec3[MAX_VERT];
+
+  num_face = 0;
+  num_facevert = 0;
+  facevert = new sgVec3[MAX_VERT];
+
+  num_meshvert = 0;
+  meshvert = new sgVec3[MAX_VERT];
 
   mode = MODE_NONE;
-  num_line = 0;
-  num_face = 0;
-  num_linevert = 0;
-  num_facevert = 0;
-  linevert = new sgVec3[MAX_VERT];
-  facevert = new sgVec3[MAX_VERT];
   num_vert = 0;
 
 /* 
   Read the next two lines of the file into INPUT1 and INPUT2. 
 */
-
   for ( ;; ) {
+
+    int   count;
+    int   code;
+    int   width;
+    char  input1[MAX_LINE_LEN];
+    char  input2[MAX_LINE_LEN];
 
 /* 
   INPUT1 should contain a single integer, which tells what INPUT2
@@ -106,49 +173,28 @@ static int dxf_read ( FILE *filein )
     if ( code == 0 ) {
 
       dxf_flush () ;
-      
-      if ( strncmp( input2, "LINE", 4 ) == 0 ) {
+
+      //set mode
+      if ( strncmp( input2, "LINE", 4 ) == 0 )
         mode = MODE_LINE ;
-        num_vert = 0 ;
-      }
-      else if ( strncmp( input2, "3DFACE", 6 ) == 0 ) {
+      else if ( strncmp( input2, "3DFACE", 6 ) == 0 )
         mode = MODE_FACE ;
-        num_vert = 0 ;
-      }
-      else if ( strncmp( input2, "POLYLINE", 8 ) == 0 ) {
+      else if ( strncmp( input2, "POLYLINE", 8 ) == 0 )
         mode = MODE_POLYLINE ;
-        num_vert = 0 ;
-      }
-      else if ( strncmp( input2, "VERTEX", 6 ) == 0 ) {
-        if ( mode != MODE_POLYLINE ) {
-          mode = MODE_NONE;
-          num_vert = 0 ;
-        }
-      }
-      else if ( strncmp( input2, "SEQEND", 6 ) == 0 ) {
-        if ( mode == MODE_POLYLINE ) {
-          num_vert = ( num_vert / 2 ) * 2 ;  //make even
-          if ( num_vert > 1 ) {
-            num_line += ( num_vert / 2 ) ;
-            num_linevert += num_vert ;
-          }
-          num_vert = 0;
-        }
+      else if ( strncmp( input2, "VERTEX", 6 ) == 0 )
+        mode = MODE_VERTEX;
+      else
         mode = MODE_NONE;
-        num_vert = 0;
-      }
-      else {
-        mode = MODE_NONE;
-        num_vert = 0;
-      }
     }
     else {
       
+      int cpos;
       for (cpos = 0; input1[cpos] == ' '; cpos++)
         ;
       
       if ( input1[cpos] == '1' || input1[cpos] == '2' || input1[cpos] == '3' ) {
 
+        float rval;
         count = sscanf ( input2, "%e%n", &rval, &width );
 
         switch ( input1[cpos] )
@@ -176,21 +222,38 @@ static int dxf_read ( FILE *filein )
                 num_vert ++ ;
               }
             }
-            else if ( mode == MODE_POLYLINE ) {
-              if ( num_linevert + num_vert < MAX_VERT ) {
-                sgCopyVec3( linevert[num_linevert + num_vert], cvec );
-                num_vert ++ ;
-              }
-              if ( num_vert >= 2 && num_linevert + num_vert < MAX_VERT ) {
-                //duplicate vertex if not first
-                sgCopyVec3( linevert[num_linevert + num_vert], cvec );
-                num_vert ++ ;
-              }
-            }
-
             break;
-        
-          default:
+        }
+      }
+      else if ( input1[cpos] == '7' ) {
+
+        int ival ;
+        count = sscanf ( input2, "%d%n", &ival, &width );
+
+        switch ( input1[cpos+1] )
+        {
+          case '0':
+            cflags = ival;
+            break;
+
+          case '1':
+            meshface[0] = ival;
+            num_vert = 1;
+            break;
+
+          case '2':
+            meshface[1] = ival;
+            num_vert = 2;
+            break;
+
+          case '3':
+            meshface[2] = ival;
+            num_vert = 3;
+            break;
+
+          case '4':
+            meshface[3] = ival;
+            num_vert = 4;
             break;
         }
       }
@@ -220,6 +283,7 @@ static int dxf_read ( FILE *filein )
 
   delete[] linevert;
   delete[] facevert;
+  delete[] meshvert;
 
   return TRUE;
 }
