bca02a8e120378bb2fbc2b1b2c11ba4ec5203342
mcdave <mcdave@713b2cfd-b11f-0410-b270-d5401603882f>
2000-07-15 23:27:17 +0000

    moved glx to local

--- trunk/src/ssg/ssgLocal.h
+++ trunk/src/ssg/ssgLocal.h
@@ -3,6 +3,30 @@
 
 #include "ssg.h"
 
+
+#ifndef WIN32
+#  ifndef macintosh
+#    include <GL/glx.h>
+#  else
+#    include <agl.h>
+#  endif
+#endif
+
+/*
+  glGetCurrentContext()
+*/
+
+#ifdef WIN32
+#  define glGetCurrentContext() wglGetCurrentContext()
+#else
+#  if defined(macintosh)
+#     define glGetCurrentContext() aglGetCurrentContext()
+#  else
+#     define glGetCurrentContext() glXGetCurrentContext()
+#  endif
+#endif
+
+
 extern void (*__ssgEnableTable[64])() ;
 extern void (*__ssgDisableTable[64])() ;
 
