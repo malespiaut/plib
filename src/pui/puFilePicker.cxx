3921cf73acd576443da9e06a75c1a8fd571aaa6c
mcdave <mcdave@713b2cfd-b11f-0410-b270-d5401603882f>
2000-08-04 17:45:57 +0000

    added listbox

---
+++ src/pui/puFilePicker.cxx
@@ -0,0 +1,130 @@
+
+#include "puLocal.h"
+
+static void handle_slider ( puObject * slider )
+{
+  float val ;
+  slider -> getValue ( &val ) ;
+  val = 1.0f - val ;
+
+  puListBox* list_box = (puListBox*) slider -> getUserData () ;
+  int index = int ( list_box -> getNumItems () * val ) ;
+  list_box -> setTopItem ( index ) ;
+}
+
+void puFilePicker::handle_select ( puObject* list_box )
+{
+  puFilePicker* file_picker = (puFilePicker*) list_box -> getUserData () ;
+
+  int selected ;
+  list_box -> getValue ( &selected ) ;
+
+  if ( selected >= 0 && selected < file_picker -> num_files )
+    file_picker -> setValue ( file_picker -> files [ selected ] ) ;
+  else
+    file_picker -> setValue ( "" ) ;
+}
+
+static void handle_button ( puObject* b )
+{
+  puFilePicker* file_picker = (puFilePicker*) b -> getUserData () ;
+  file_picker -> invokeCallback () ;
+}
+
+puFilePicker::puFilePicker ( int x, int y, const char* dir ) : puDialogBox ( x, y )
+{
+  files = 0 ;
+  num_files = 0 ;
+  setValue ( "" ) ;
+
+  find_files ( dir ) ;
+
+  new puFrame ( 0, 0, 220, 170 );
+
+  puSlider* slider = new puSlider (170+10,40,100,TRUE);
+  slider->setDelta(0.1f);
+  slider->setValue(1.0f);
+  slider->setSliderFraction (0.2f) ;
+  slider->setCBMode( PUSLIDER_DELTA );
+  
+  puListBox* list_box = new puListBox ( 20, 40, 170, 140, files ) ;
+  list_box -> setLabel ( "Pick a file" );
+  list_box -> setLabelPlace ( PUPLACE_ABOVE ) ;
+  list_box -> setStyle ( -PUSTYLE_SMALL_SHADED ) ;
+  list_box -> setValue ( 0 ) ;
+
+  list_box -> setUserData ( this ) ;
+  list_box -> setCallback ( handle_select ) ;
+  
+  slider -> setUserData ( list_box ) ;
+  slider -> setCallback ( handle_slider ) ;
+
+  puOneShot* cancel_button = new puOneShot ( 20, 10, 90, 30 ) ;
+  cancel_button -> setLegend ( "Cancel" ) ;
+  cancel_button -> setUserData ( this ) ;
+  cancel_button -> setCallback ( handle_button ) ;
+  
+  puOneShot* ok_button = new puOneShot ( 100, 10, 170, 30 ) ;
+  ok_button -> setLegend ( "Ok" ) ;
+  ok_button -> setUserData ( this ) ;
+  ok_button -> setCallback ( handle_button ) ;
+//  ok_button->makeReturnDefault ( TRUE ) ;
+
+  close  () ;
+  reveal () ;
+}
+
+puFilePicker::~puFilePicker ()
+{
+  if ( files )
+  {
+    for ( int i=0; i<num_files; i++ )
+    {
+      delete files[i];
+      files[i] = 0;
+    }
+    delete[] files;
+    files = 0;
+  }
+  num_files = 0;
+}
+
+void puFilePicker::find_files ( const char* dir )
+{
+  num_files = 0;
+  for ( int pass=0; pass<2; pass++ )
+  {
+    int ifile = 0;
+    
+    ulDir* dirp = ulOpenDir(dir);
+    if ( dirp != NULL )
+    {
+      ulDirEnt* dp;
+      while ( (dp = ulReadDir(dirp)) != NULL )
+      {
+        if ( ! dp->d_isdir )
+        {
+          if ( pass )
+          {
+            files[ ifile ] = new char[ strlen(dp->d_name)+1 ];
+            strcpy( files[ ifile ], dp->d_name );
+          }
+          
+          ifile ++;
+        }
+      }
+      ulCloseDir(dirp);
+    }
+    
+    if ( pass == 0 )
+    {
+      num_files = ifile;
+      if ( num_files == 0 )
+        return;
+      
+      files = new char* [ num_files+1 ];
+      for ( int i=0; i<=num_files; i++ )
+        files [i] = 0 ;
+    }
+  }
+}
---
+++ src/pui/puListBox.cxx
@@ -0,0 +1,97 @@
+
+#include "puLocal.h"
+
+puListBox::puListBox  ( int minx, int miny, int maxx, int maxy, char** _list ) :
+               puButton ( minx, miny, maxx, maxy )
+{
+  type |= PUCLASS_LISTBOX ;
+
+  list = _list ;
+  for ( num = 0 ; list [ num ] != NULL ; num++ )
+    /* Count number of items */ ;
+  top = 0 ;
+
+  /* Set index of selected item */
+  setValue ( -1 ) ;
+}
+
+
+void puListBox::setTopItem( int item_index )
+{
+  top = item_index ;
+  if ( top < 0 )
+    top = 0 ;
+  else if ( top > num-1 )
+    top = num-1;
+}
+
+
+void puListBox::draw ( int dx, int dy )
+{
+  if ( !visible ) return ;
+
+  abox . draw ( dx, dy, style, colour, isReturnDefault() ) ;
+
+  /* If greyed out then halve the opacity when drawing the label and legend */
+
+  if ( active )
+    glColor4fv ( colour [ PUCOL_LEGEND ] ) ;
+  else
+    glColor4f ( colour [ PUCOL_LEGEND ][0],
+                colour [ PUCOL_LEGEND ][1],
+                colour [ PUCOL_LEGEND ][2],
+                colour [ PUCOL_LEGEND ][3] / 2.0f ) ; /* 50% more transparent */
+
+  if ( r_cb )
+    r_cb ( this, render_data ) ;
+  else
+  {
+    int ysize = abox.max[1] - abox.min[1] + 1 ;
+    int yinc = puGetStringHeight(legendFont) + PUSTR_BGAP ;
+    int num_vis = (ysize - PUSTR_BGAP) / yinc ;
+
+    int selected ;
+    getValue ( &selected ) ;
+
+    for ( int i=top; i<num && i<top+num_vis; i++ )
+    {
+      if ( i == selected )
+        glColor4f ( 1.0, 1.0, 1.0, 1.0 ) ;
+      else
+        glColor4f ( 0.0, 0.0, 0.0, 1.0 ) ;
+
+      int x = PUSTR_LGAP ;
+      int y = PUSTR_BGAP + yinc * ((i-top)+1) ;
+
+      int xx = dx + abox.min[0] + x ;
+      int yy = dy + abox.max[1] - y ;
+
+      const char* str = list [ i ] ;
+      puDrawString ( legendFont, (char*)str, xx, yy ) ;
+    }
+  }
+
+  draw_label ( dx, dy ) ;
+}
+
+
+void puListBox::doHit ( int button, int updown, int x, int y )
+{
+  if ( button == PU_LEFT_BUTTON )
+  {
+    lowlight () ;
+    
+    int yinc = puGetStringHeight(legendFont) + PUSTR_BGAP ;
+    int index = top + ( abox.max[1] - PUSTR_BGAP - y ) / yinc;
+    if ( index < 0 )
+      index = 0;
+    else if ( index >= num )
+      index = num-1;
+    
+    setValue ( index ) ;
+    
+    invokeCallback () ;
+  }
+  else
+    lowlight () ;
+}
