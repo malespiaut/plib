a6de494c70a8c338f823cb8e5c023ebc4ea7343c
mcdave <mcdave@713b2cfd-b11f-0410-b270-d5401603882f>
2000-08-31 18:50:57 +0000

    Michael Kurth's Quat VS Euler example

--- trunk/examples/src/sg/sg_quat_test.cxx
+++ trunk/examples/src/sg/sg_quat_test.cxx
@@ -1,10 +1,272 @@
-
 /*
-  This program is no more - it is an ex-program.
+  This is a demonstration of Quaternion vs. Euler rotation
+
+  The cube on the left uses Euler rotation. The cube on
+  the right uses Quaternion rotation. As you can see the
+  Euler cube is in midst of a gimble-lock-death-throw,
+  while the Quaternion cube is happily spinning away.
+
+  I have setup two rows of keys to control the cubes rotations.
+
+  Quaternion Rotation Toggles:
+  q - toggle X axis rotation
+  w - toggle Y axis rotation
+  e - toggle Z axis rotation
+  r - reset quaternion to initial state
+  t - untoggle all rotations
+
+  Euler Rotation Toggles:
+  a - toggle X axis rotation
+  s - toggle Y axis rotation
+  d - toggle Z axis rotation
+  f - reset euler to initial state
+  g - untoggle all rotations
+
+  ESC exits
+
+  Written by Michael Kurth (kurth@futurekill.com) (negative0@earthlink.net)
 */
-  
-int main ( int, char ** )
+
+#include <GL/glut.h>
+
+#include "plib/sg.h"
+
+#define XSIZE 320
+#define YSIZE 320
+
+sgVec3 Euler ;
+sgVec3 EulerPos ;
+sgVec3 EulerRot ;
+
+sgVec3 Quat ;
+sgVec3 QuatPos ;
+sgQuat QuatRot ;
+
+static GLfloat cube_vertices[] =
 {
-  return 0 ;
+ -1.0, -1.0,  1.0,  1.0, 0.0, 0.0,
+  1.0, -1.0,  1.0,  0.5, 0.5, 0.0,
+  1.0,  1.0,  1.0,  0.0, 1.0, 0.0,
+ -1.0,  1.0,  1.0,  0.0, 0.5, 0.5,
+ -1.0, -1.0, -1.0,  0.0, 0.0, 1.0,
+  1.0, -1.0, -1.0,  0.5, 0.0, 0.5,
+  1.0,  1.0, -1.0,  1.0, 0.0, 0.0,
+ -1.0,  1.0, -1.0,  0.5, 0.5, 0.0
+} ;
+
+static GLubyte frontIndices[] = { 0, 1, 2, 3 } ;
+static GLubyte backIndices[] = { 5, 4, 7, 6 } ;
+
+static GLubyte rightIndices[] = { 1, 5, 6, 2 } ;
+static GLubyte leftIndices[] = { 4, 0, 3, 7 } ;
+
+static GLubyte topIndices[] = { 3, 2, 6, 7 } ;
+static GLubyte bottomIndices[] = { 4, 5, 1, 0 } ;
+
+void InitEulerCube( )
+{
+  sgSetVec3( Euler, 1, 1, 1 ) ;
+  sgSetVec3( EulerRot, 0, 0, 0 ) ;
+  sgSetVec3( EulerPos, -2, 0, -5 ) ;
 }
 
+void RotateEulerCube( )
+{
+  if( Euler[ SG_X ] )
+  {
+    EulerRot[ SG_X ] += 5.0 ;
+    if( EulerRot[ SG_X ] > 360. )
+      EulerRot[ SG_X ] = 0.0 ;
+  }
+  
+  if( Euler[ SG_Y ] )
+  {
+    EulerRot[ SG_Y ] += 5.0 ;
+    if( EulerRot[ SG_Y ] > 360. )
+      EulerRot[ SG_Y ] = 0.0 ;
+  }
+  
+  if( Euler[ SG_Z ] )
+  {
+    EulerRot[ SG_Z ] += 5.0 ;
+    if( EulerRot[ SG_Z ] > 360. )
+      EulerRot[ SG_Z ] = 0.0 ;
+  }
+  
+}
+
+void InitQuatCube( )
+{
+  sgSetVec3( Quat, 1, 1, 1 ) ;
+  sgMakeIdentQuat( QuatRot ) ;
+  sgSetVec3( QuatPos, 2, 0, -5 ) ;
+}
+
+void RotateQuatCube( )
+{
+  if( Quat[ SG_X ] )
+    sgRotQuat( QuatRot, 5.0, 1.0, 0.0, 0.0 ) ;
+  
+  if( Quat[ SG_Y ] )
+    sgRotQuat( QuatRot, 5.0, 0.0, 1.0, 0.0 ) ;
+  
+  if( Quat[ SG_Z ] )
+    sgRotQuat( QuatRot, 5.0, 0.0, 0.0, 1.0 ) ;
+}
+
+
+void DrawCube( )
+{
+  glDrawElements( GL_QUADS, 4, GL_UNSIGNED_BYTE, frontIndices ) ;
+  glDrawElements( GL_QUADS, 4, GL_UNSIGNED_BYTE, backIndices ) ;
+  glDrawElements( GL_QUADS, 4, GL_UNSIGNED_BYTE, rightIndices ) ;
+  glDrawElements( GL_QUADS, 4, GL_UNSIGNED_BYTE, leftIndices ) ;
+  glDrawElements( GL_QUADS, 4, GL_UNSIGNED_BYTE, topIndices ) ;
+  glDrawElements( GL_QUADS, 4, GL_UNSIGNED_BYTE, bottomIndices ) ;
+}
+
+void Display( void )
+{
+  sgMat4 matrix ;
+  
+  glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT ) ;
+  
+  glLoadIdentity( ) ;
+  glTranslatef( 0, 0, -5 ) ;
+  
+  glPushMatrix( ) ;
+  {
+    glTranslatef( QuatPos[ SG_X ], QuatPos[ SG_Y ], QuatPos[ SG_Z ] ) ;
+    
+    sgQuatToMatrix( matrix, QuatRot ) ;
+    glMultMatrixf( (float*)matrix ) ;
+    
+    DrawCube( ) ;
+  }
+  glPopMatrix( ) ;
+  
+  glPushMatrix( ) ;
+  {
+    glTranslatef( EulerPos[ SG_X ], EulerPos[ SG_Y ], EulerPos[ SG_Z ] ) ;
+    
+    sgMakeRotMat4( matrix, EulerRot ) ;
+    glMultMatrixf( (float*)matrix ) ;
+    
+    DrawCube( ) ;
+  }
+  glPopMatrix( ) ;
+  
+  
+  glutSwapBuffers( ) ;
+}
+
+void Idle( )
+{
+  static int lastTime = 0;
+  int time = glutGet(GLUT_ELAPSED_TIME);
+  if (time > lastTime + 10)
+  {
+    RotateQuatCube( ) ;
+    RotateEulerCube( ) ;
+    
+    lastTime = time;
+  }
+  glutPostRedisplay( ) ;
+}
+
+void Keyboard( unsigned char key, int xPos, int yPos )
+{
+  switch( key )
+  {
+  case 27:
+    exit( 0 ) ;
+  case 'q':
+    Quat[ SG_X ] = !Quat[ SG_X ] ;
+    break ;
+  case 'w':
+    Quat[ SG_Y ] = !Quat[ SG_Y ] ;
+    break ;
+  case 'e':
+    Quat[ SG_Z ] = !Quat[ SG_Z ] ;
+    break ;
+  case 'r':
+    InitQuatCube( ) ;
+    break ;
+  case 't':
+    sgSetVec3( Quat, 0, 0, 0 ) ;
+    break ;
+    
+  case 'a':
+    Euler[ SG_X ] = !Euler[ SG_X ] ;
+    break ;
+  case 's':
+    Euler[ SG_Y ] = !Euler[ SG_Y ] ;
+    break ;
+  case 'd':
+    Euler[ SG_Z ] = !Euler[ SG_Z ] ;
+    break ;
+  case 'f':
+    InitEulerCube( ) ;
+    break ;
+  case 'g':
+    sgSetVec3( Euler, 0, 0, 0 ) ;
+    break ;
+  }
+}
+
+void Reshape( int w, int h )
+{
+  
+  if( h == 0 )
+  {
+    h = 1 ;
+  }
+  
+  glViewport( 0, 0, w, h ) ;
+  
+  glMatrixMode( GL_PROJECTION ) ;
+  glLoadIdentity( ) ;
+  
+  gluPerspective( 45.0f, (GLfloat)w / (GLfloat)h, 0.1f, 100.0f ) ;
+  
+  glMatrixMode( GL_MODELVIEW ) ;
+  glLoadIdentity( ) ;
+}
+
+void Init( void )
+{
+  glClearColor( 0.0, 0.0, 0.0, 0.0 ) ;
+  
+  glEnable(GL_DEPTH_TEST);
+  glCullFace(GL_BACK);
+  glEnable(GL_CULL_FACE);
+  
+  glEnableClientState( GL_VERTEX_ARRAY ) ;
+  glEnableClientState( GL_COLOR_ARRAY ) ;
+  
+  glVertexPointer( 3, GL_FLOAT, 6*sizeof(GLfloat), cube_vertices ) ;
+  glColorPointer(  3, GL_FLOAT, 6*sizeof(GLfloat), &cube_vertices[3] ) ;
+  
+  InitEulerCube( ) ;
+  InitQuatCube( ) ;
+  
+  Reshape( XSIZE, YSIZE ) ;
+}
+
+void  main( int argc, char **argv )
+{
+  glutInit( &argc, argv ) ;
+  glutInitDisplayMode( GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH ) ;
+  glutInitWindowSize( XSIZE, YSIZE ) ;
+  glutCreateWindow( "Quaternion Demo" ) ;
+  
+  Init( ) ;
+  
+  glutDisplayFunc( Display ) ;
+  glutReshapeFunc( Reshape ) ;
+  glutKeyboardFunc( Keyboard ) ;
+  glutIdleFunc( Idle ) ;
+  
+  glutShowWindow( ) ;
+  glutMainLoop( ) ;
+}
