ae9ff7db57bd14fb74f912bf1815414adea2128f
mcdave <mcdave@713b2cfd-b11f-0410-b270-d5401603882f>
2000-09-06 22:18:31 +0000

    created viewer

--- trunk/examples/src/ssg/viewer/viewer.cxx
+++ trunk/examples/src/ssg/viewer/viewer.cxx
@@ -8,39 +8,157 @@
 #include <unistd.h>
 #endif
 #include <math.h>
-#include <plib/ssg.h>
+
 #include <GL/glut.h>
 
-ssgRoot      *scene  = NULL ;
-ssgTransform *object = NULL ;
-
+#include <plib/ssg.h>
+#include <plib/fnt.h>
+#include <plib/pu.h>
 
 /*
-  Something to make some interesting motion
-  for both Object and the camera.
+scene graph
 */
+ssgRoot *scene = NULL ;
 
-void update_motion ()
+/*
+font vars
+*/
+fntRenderer *text ;
+fntTexFont *font ;
+
+puFilePicker* file_picker = 0 ;
+
+/*
+frame rate vars
+*/
+int frames = 0;
+float fps = 0;
+
+/*
+animation vars
+*/
+#define MAX_SPEED 60
+static const int speed_tab [] =
+{ 0, 1, 2, 3, 4, 5, 6, 10, 12, 15, 20, 30, 60 };
+
+int speed_index = 0 ;
+int anim_delay = -1 ;
+
+/*
+spinner vars
+*/
+int downx, downy;   /* for tracking mouse position */
+int downb = -1;     /* and button status */
+
+GLfloat downDist, downEl, downAz, /* for saving state of things */
+downEx, downEy, downEz;   /* when button is pressed */
+
+GLfloat dAz, dEl, lastAz, lastEl;  /* to calculate spinning w/ polar motion */
+GLfloat AzSpin = 0.0f, ElSpin = 0.0f;
+int     AdjustingAzEl = 0;
+
+/* Minimum spin to allow in polar (lower forced to zero) */
+#define MIN_AZSPIN 0.1f
+#define MIN_ELSPIN 0.1f
+
+/* Factors used in computing dAz and dEl (which determine AzSpin, ElSpin) */
+#define SLOW_DAZ 0.90f
+#define SLOW_DEL 0.90f
+#define PREV_DAZ 0.80f
+#define PREV_DEL 0.80f
+#define CUR_DAZ  0.20f
+#define CUR_DEL  0.20f
+
+/*
+*  polar movement parameters
+*/
+GLfloat EyeDist= 100.0f;
+GLfloat EyeAz  = 0.0f;
+GLfloat EyeEl  = 30.0f;
+GLfloat Ex     = 0.0f;
+GLfloat Ey     = 0.0f;
+GLfloat Ez     = 0.0f;
+
+#define FOV 60.0f
+
+int getWindowHeight () { return glutGet ( (GLenum) GLUT_WINDOW_HEIGHT ) ; }
+int getWindowWidth  () { return glutGet ( (GLenum) GLUT_WINDOW_WIDTH  ) ; }
+
+static void begin2d ( void )
 {
-  static int frameno = 0 ;
-
-  frameno++ ;
-
-  sgCoord campos ;
-  sgCoord objpos ;
-
-  /*
-    Spin Object, make the camera pan sinusoidally left and right
-  */
-
-  sgSetCoord ( & campos, 0.0f, -50.0f, 1.0f,
-                          25.0 * sin(frameno/100.0), 0.0f, 0.0f ) ;
-  sgSetCoord ( & objpos, 0.0f,  0.0f, 0.0f, frameno, 0.0f, 0.0f ) ;
-
-  ssgSetCamera ( & campos ) ;
-  object -> setTransform ( & objpos ) ;
+  int w = getWindowWidth  () ;
+  int h = getWindowHeight () ;
+  
+  glDisable      ( GL_LIGHTING ) ;
+  glDisable      ( GL_TEXTURE_2D ) ;
+  glDisable      ( GL_DEPTH_TEST ) ;
+  glDisable      ( GL_CULL_FACE  ) ;
+  glEnable       ( GL_ALPHA_TEST ) ;
+  glEnable       ( GL_BLEND ) ;
+  glAlphaFunc    ( GL_GREATER, 0.1 ) ;
+  glBlendFunc    ( GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA ) ;
+  
+  //create an projection that acts like a 2D screen
+  glMatrixMode   ( GL_PROJECTION ) ;
+  glPushMatrix   () ;
+  glLoadIdentity () ;
+  glOrtho        ( 0, w, 0, h, 0, 1 ) ;
+  glMatrixMode   ( GL_MODELVIEW ) ;
+  glPushMatrix   () ;
+  glLoadIdentity () ;
 }
 
+static void end2d ( void )
+{
+  glMatrixMode   ( GL_PROJECTION ) ;
+  glPopMatrix    () ;
+  glMatrixMode   ( GL_MODELVIEW ) ;
+  glPopMatrix    () ;
+  
+  glDisable      ( GL_ALPHA_TEST ) ;
+  glDisable      ( GL_BLEND ) ;
+  glAlphaFunc    ( GL_ALWAYS, 0.0 ) ;
+  glBlendFunc    ( GL_ONE, GL_ZERO ) ;
+}
+
+void show_frame( ssgEntity *e )
+{
+  if ( e -> isAKindOf ( SSG_TYPE_BRANCH ) )
+  {
+    ssgBranch *br = (ssgBranch *) e ;
+    
+    for ( int i = 0 ; i < br -> getNumKids () ; i++ )
+      show_frame ( br -> getKid ( i ) ) ;
+    
+    if ( e -> isAKindOf ( SSG_TYPE_TIMEDSELECTOR ) )
+    {
+      ssgTimedSelector* p = (ssgTimedSelector*) e ;
+      int frame = p -> getStep () ;	
+      char buffer [40];
+      sprintf ( buffer, "frame : %-4d\n", frame ) ;
+      text -> puts ( buffer ) ;
+    }
+  }
+}
+
+
+void make_matrix( sgMat4 mat )
+{
+  SGfloat angle = -EyeAz * SG_DEGREES_TO_RADIANS ;
+  sgVec3 eye ;
+  eye[0] = (SGfloat) cos (angle) * EyeDist ;
+  eye[1] = (SGfloat) sin (angle) * EyeDist ;
+  angle = EyeEl * SG_DEGREES_TO_RADIANS ;
+  eye[2] = (SGfloat) sin (angle) * EyeDist ;
+  
+  sgVec3 center ;
+  sgSetVec3 ( center, Ex, Ey, Ez ) ;
+  
+  sgVec3 up ;
+  sgSetVec3 ( up, 0.0f, 0.0f, 0.1f ) ;
+  
+  sgMakeLookAtMat4 ( mat, eye, center, up ) ;
+}
 
 
 /*
@@ -53,36 +171,370 @@ void reshape ( int w, int h )
 }
 
 
-
 /*
-  The GLUT keyboard event
+  The GLUT display event
 */
 
-void keyboard ( unsigned char, int, int )
+void display(void)
 {
-  exit ( 0 ) ;
-}
+  glClearColor ( 0.2f, 0.7f, 1.0f, 1.0f ) ;
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+  
+  sgMat4 mat ;
+  make_matrix ( mat ) ;
+  ssgSetCamera ( mat );
+  
+  glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+  glEnable(GL_BLEND);
+  glEnable( GL_DEPTH_TEST ) ;
+  
+  int frameCounter = ssgGetFrameCounter () ;
+  
+  /*
+  increment the frame counter
+  */
+  static int last_time = 0;
+  int curr_time = glutGet(GLUT_ELAPSED_TIME);
+  while (curr_time >= last_time + 1000/60)
+  {
+    last_time += 1000/60;
 
+    static int anim_time = 0 ;
+    if ( anim_delay != -1 && ++ anim_time >= anim_delay )
+    {
+      ssgSetFrameCounter ( ++ frameCounter ) ;
+      anim_time = 0 ;
+    }
+  }
 
-
-/*
-  The GLUT redraw event
-*/
-
-void redraw ()
-{
-  glutWarpPointer ( 320, 200 ) ;
-  update_motion () ;
-
-  glClear  ( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT ) ;
-
+  /*
+  figure out how fast this bus is going
+  */
+  static int last_time2 = 0;
+  if (curr_time >= last_time2 + 2000) {
+    fps = frames * 1000.0f / (curr_time - last_time2);
+    frames = 0;
+    last_time2 = curr_time;
+  }
+  
   ssgCullAndDraw ( scene ) ;
+
+  /*
+  restore frameCounter since ssgCullAndDraw() changes it
+  */
+  ssgSetFrameCounter ( frameCounter ) ;
+
+  /*
+  do the interface stuff
+  */
+  int w = getWindowWidth  () ;
+  int h = getWindowHeight () ;
+  
+  begin2d () ;
+  
+  text -> setFont      ( font ) ;
+  text -> setPointSize ( 12 ) ;
+  
+  if ( scene -> getNumKids() == 0 )
+  {
+    text -> begin () ;
+    glColor3f ( 0, 0, 0 ) ;
+    text -> start2f ( (float)4, (float)(h-16) ) ;
+    text -> puts ( "controls\n" );
+    text -> puts ( " <esc> : exit\n" ) ;
+    text -> puts ( " c : clear\n" ) ;
+    text -> puts ( " l : load a shape or sequence\n" ) ;
+    text -> puts ( " <key.pad.plus> : faster animation\n" ) ;
+    text -> puts ( " <key.pad.minus> : slower animation\n" ) ;
+    text -> puts ( " <left.mouse.button> : rotate\n" ) ;
+    text -> puts ( " <right.mouse.button> : zoom\n" ) ;
+    text -> end () ;
+  }
+  else if ( fps > 0 )
+  {
+    char buffer [ 80 ] ;
+    text -> begin () ;
+    glColor3f ( 0, 0, 0 ) ;
+    text -> start2f ( (float)4, (float)(h-16) ) ;
+    sprintf ( buffer, "fps : %.02f\n", fps ) ;
+      text -> puts ( buffer ) ;
+    sprintf ( buffer, "dist : %.02f\n", EyeDist ) ;
+      text -> puts ( buffer ) ;
+    sprintf ( buffer, "anim_speed : %d fps\n", speed_tab[ speed_index ] ) ;
+      text -> puts ( buffer ) ;
+    sprintf ( buffer, "time : %-4d, %d\n", curr_time, frameCounter ) ;
+      text -> puts ( buffer ) ;
+    show_frame( scene ) ;
+    text -> end () ;
+  }
+  
+  glColor3f ( 1, 1, 1 ) ;
+  end2d () ;
+  
+  frames ++;
+  
+  puDisplay () ;
 
   glutPostRedisplay () ;
   glutSwapBuffers () ;
 }
 
+/*
+* keep them vertical; I think this makes a lot of things easier, 
+* but maybe it wouldn't be too hard to adapt things to let you go
+* upside down
+*/
+int ConstrainEl(void)
+{
+  if (EyeEl <= -90) {
+    EyeEl = -89.99f;
+    return 1;
+  } else if (EyeEl >= 90) {
+    EyeEl = 89.99f;
+    return 1;
+  }
+  return 0;
+}
 
+/* What to multiply number of pixels mouse moved by to get rotation amount */
+#define EL_SENS   0.5f
+#define AZ_SENS   0.5f
+
+void motion(int x, int y)
+{
+  puMouse ( x, y ) ;
+  
+  int deltax = x - downx, deltay = y - downy;
+  
+  //  if ( scene -> getNumKids() > 0 && !file_picker )
+  if ( !file_picker )
+    
+    switch (downb) {
+  case GLUT_LEFT_BUTTON:
+    EyeEl  = downEl + EL_SENS * deltay;
+    ConstrainEl();
+    EyeAz  = downAz + AZ_SENS * deltax;
+    dAz    = PREV_DAZ*dAz + CUR_DAZ*(lastAz - EyeAz);
+    dEl    = PREV_DEL*dEl + CUR_DEL*(lastEl - EyeEl);
+    lastAz = EyeAz;
+    lastEl = EyeEl;
+    break;
+  case GLUT_RIGHT_BUTTON:
+    {
+      float mult = 0.01f;
+      if ( fabs(downDist) > 1.0f )
+        mult = float( 0.001 * fabs(downDist) );
+      EyeDist = downDist + mult*deltay;
+      break;
+    }
+  case GLUT_MIDDLE_BUTTON:
+    {
+      float mult = 0.01f;
+      if ( fabs(EyeDist) > 1.0f )
+        mult = float( 0.0005f * fabs(EyeDist) );
+      
+      sgMat4 mat ;
+      make_matrix( mat ) ;
+      
+      sgVec3 off;
+      sgSetVec3( off, mult*deltax, 0, -mult*deltay );
+      sgXformVec3( off, mat );
+      
+      Ex -= off[0];
+      Ey -= off[1];
+      Ez -= off[2];
+    }
+    break;
+  }
+}
+
+void mouse(int button, int state, int x, int y)
+{
+  puMouse ( button, state, x, y ) ;
+  
+  if (state == GLUT_DOWN && downb == -1) {
+    downx = x;
+    downy = y;
+    downb = button;
+    
+    //    if ( scene -> getNumKids() > 0 && !file_picker )
+    if (!file_picker )
+      switch ( button )
+    {
+    case GLUT_LEFT_BUTTON:
+      lastEl = downEl = EyeEl;
+      lastAz = downAz = EyeAz;
+      AzSpin = ElSpin = dAz = dEl = 0;
+      AdjustingAzEl = 1;
+      break;
+    case GLUT_RIGHT_BUTTON:
+      downDist = EyeDist;
+      break;
+    case GLUT_MIDDLE_BUTTON:
+      downEx = Ex;
+      downEy = Ey;
+      downEz = Ez;
+      break;
+    }
+  } else if (state == GLUT_UP && button == downb) {
+    downb = -1;
+    
+    //    if ( scene -> getNumKids() > 0 && !file_picker )
+    if ( !file_picker )
+      switch ( button )
+    {
+    case GLUT_LEFT_BUTTON:
+      AzSpin =  -dAz;
+      if (AzSpin < MIN_AZSPIN && AzSpin > -MIN_AZSPIN)
+        AzSpin = 0;	
+      ElSpin = -dEl;
+      if (ElSpin < MIN_ELSPIN && ElSpin > -MIN_ELSPIN)
+        ElSpin = 0; 
+      AdjustingAzEl = 0;
+      break;
+    }
+  }
+}
+
+void special(int key, int x, int y)
+{
+  puKeyboard ( key + PU_KEY_GLUT_SPECIAL_OFFSET, PU_DOWN ) ;
+  
+  switch ( key )
+  {
+  case GLUT_KEY_LEFT:
+  case GLUT_KEY_UP:
+  case GLUT_KEY_RIGHT:
+  case GLUT_KEY_DOWN:
+  case GLUT_KEY_PAGE_UP:
+  case GLUT_KEY_PAGE_DOWN:
+  case GLUT_KEY_HOME:
+  case GLUT_KEY_END:
+    break;
+  }
+}
+
+void start_anim( ssgEntity *e )
+{
+  if ( e -> isAKindOf ( SSG_TYPE_BRANCH ) )
+  {
+    ssgBranch *br = (ssgBranch *) e ;
+    
+    for ( int i = 0 ; i < br -> getNumKids () ; i++ )
+      start_anim ( br -> getKid ( i ) ) ;
+    
+    if ( e -> isAKindOf ( SSG_TYPE_TIMEDSELECTOR ) )
+    {
+      ssgTimedSelector* p = (ssgTimedSelector*) e ;
+      p -> setMode ( SSG_ANIM_SHUTTLE ) ;
+      p -> control ( SSG_ANIM_START ) ;
+    }
+  }
+}
+
+
+void pick_cb ( puObject * )
+{
+  char* str ;
+  file_picker -> getValue ( &str ) ;
+  
+  char fname [80];
+  strcpy ( fname, str ) ;
+  
+  puDeleteObject ( file_picker ) ;
+  file_picker = 0 ;
+
+  ssgEntity *obj = ssgLoad ( (char*)fname ); //"test_Med.ase" ) ;
+  
+  if ( !obj )
+    return;
+  
+  start_anim( obj );
+  
+  scene -> addKid ( obj ) ;
+  
+  SGfloat radius = scene->getBSphere()->getRadius();
+  EyeDist = float( radius * 1.5f / tan( float( FOV/2 * SG_DEGREES_TO_RADIANS ) ) );
+  
+  sgSphere sp = *( scene -> getBSphere() ) ;
+  Ex = - sp.getCenter()[ 0 ] ;
+  Ey = - sp.getCenter()[ 1 ] ;
+  Ez = - sp.getCenter()[ 2 ] ;
+}
+
+
+int get_delay( int speed_index )
+{
+  int fps = speed_tab[ speed_index ] ;
+  if ( fps > 0 )
+    return MAX_SPEED / fps ;
+  return -1 ;
+}
+
+
+/*
+  The GLUT keyboard event
+*/
+
+void keyboard(unsigned char key, int, int)
+{
+  puKeyboard ( key, PU_DOWN ) ;
+  
+  switch ( key )
+  {
+  case 0x1b:  //escape
+    exit(0);
+    break;
+  case '+':
+    if ( speed_tab[ speed_index ] != MAX_SPEED )
+      speed_index ++ ;
+    anim_delay = get_delay( speed_index );
+    break;
+  case '-':
+    if ( speed_tab[ speed_index ] != 0 )
+      speed_index -- ;
+    anim_delay = get_delay( speed_index );
+    break;
+  case ' ':
+    Ex = 0;
+    Ey = 0;
+    Ez = 0;
+    break;
+  case 'l':
+    if ( !file_picker )
+    {
+      file_picker = new puFilePicker ( ( 640 - 220 ) / 2, ( 480 - 170 ) / 2, "data" ) ;
+      file_picker -> setCallback ( pick_cb ) ;
+    }
+    break;
+  case 'c':
+    //scene -> removeKid ( obj );
+    scene -> removeAllKids () ;
+    break ;
+  }
+}
+
+void idle(void)
+{
+  EyeEl += ElSpin;
+  EyeAz += AzSpin;
+  
+  /*
+  * weird spin thing to make things look
+  * look better when you are kept from going
+  * upside down while spinning - Isn't great
+  */
+  if (ConstrainEl()) {
+    ElSpin = -ElSpin;
+    if (fabs(ElSpin) > fabs(AzSpin))
+      AzSpin = float( fabs(ElSpin) * ((AzSpin > 0) ? 1 : -1) );
+  }
+  
+  if (AdjustingAzEl) {
+    dAz *= SLOW_DAZ;
+    dEl *= SLOW_DEL;
+  }
+}
 
 void init_graphics ()
 {
@@ -96,94 +548,83 @@ void init_graphics ()
     Initialise GLUT
   */
 
-  glutInitWindowPosition ( 0, 0 ) ;
-  glutInitWindowSize     ( 640, 480 ) ;
-  glutInit               ( &fake_argc, fake_argv ) ;
-  glutInitDisplayMode    ( GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH ) ;
-  glutCreateWindow       ( fake_argv[1] ) ;
-  glutDisplayFunc        ( redraw   ) ;
-  glutReshapeFunc        ( reshape  ) ;
-  glutKeyboardFunc       ( keyboard ) ;
- 
-  /*
-    Initialise SSG
-  */
-
-  ssgInit () ;
-
+  glutInit              ( &fake_argc, fake_argv ) ;
+  glutInitWindowSize    ( 640, 480 ) ;
+  glutInitDisplayMode   ( GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH ) ;
+  glutCreateWindow      ( fake_argv[1] ) ;
+  glutDisplayFunc       ( display  ) ;
+  glutReshapeFunc       ( reshape  ) ;
+  glutKeyboardFunc      ( keyboard ) ;
+  glutMouseFunc         ( mouse    ) ;
+  glutMotionFunc        ( motion   ) ;
+  glutSpecialFunc       ( special  ) ;
+  glutIdleFunc          ( idle     ) ;
+  
   /*
     Some basic OpenGL setup
   */
 
-  glClearColor ( 0.2f, 0.7f, 1.0f, 1.0f ) ;
-  glEnable ( GL_DEPTH_TEST ) ;
-
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+  glMatrixMode(GL_MODELVIEW);
+  glLoadIdentity();
+  
+  glDepthFunc(GL_LESS);
+  glEnable(GL_DEPTH_TEST);
+  glShadeModel(GL_SMOOTH);
+  
   /*
-    Set up the viewing parameters
+  Initialise PLIB
   */
-
-  ssgSetFOV     ( 60.0f, 0.0f ) ;
-  ssgSetNearFar ( 1.0f, 700.0f ) ;
-
+  
+  ssgInit () ;
+  puInit () ;
+  
+  /*
+  Set up the viewing parameters
+  */
+  
+  ssgSetFOV     ( FOV, 0.0f ) ;
+  ssgSetNearFar ( 2.0f, 10000.0f ) ;
+  
   /*
     Set up the Sun.
   */
 
   sgVec3 sunposn ;
-  sgSetVec3 ( sunposn, 0.2f, -0.5f, 0.5f ) ;
+  sgSetVec3 ( sunposn, 200.0f, -500.0f, 500.0f ) ;
   ssgGetLight ( 0 ) -> setPosition ( sunposn ) ;
-}
-
-
-/*
-  Load a simple database
-*/
-
-void load_database ()
-{
+  
   /*
-    Set up the path to the data files
+  Set up the path to the data files
   */
-
+  
   ssgModelPath   ( "data" ) ;
   ssgTexturePath ( "data" ) ;
-
+  
   /*
-    Create a root node - and a transform to position
-    the object beneath that (in the tree that is).
+  Create a root node
   */
-
-  scene  = new ssgRoot      ;
-  object = new ssgTransform ;
-
+  
+  scene = new ssgRoot      ;
+  
   /*
-    Load the models - optimise them a bit
-    and then add them into the scene.
+  ** Set up PU
   */
-
-  ssgEntity *obj_obj = ssgLoad3ds ( "plane.3ds" ) ;
-
-  object -> addKid ( obj_obj ) ;
-
-  //ssgFlatten       ( obj_obj ) ;
-  //ssgStripify      ( object  ) ;
-  scene -> addKid ( object ) ;
-  scene -> print () ;
-}
-
+  text = new fntRenderer () ;
+  font = new fntTexFont ( "data/sorority.txf" ) ;
+  
+  puFont font1 ( font, 12 ) ;
+  puFont font2 ( font, 15 ) ;
+  puSetDefaultFonts        ( font1, font2 ) ;
+  puSetDefaultStyle        ( PUSTYLE_SMALL_SHADED ) ;
+  puSetDefaultColourScheme ( 0.7f, 0.7f, 0.7f, 1.0f ) ;
+} 
 
 
-/*
-  The works.
-*/
-
-int main ( int, char ** )
+int main(int argc, char** argv)
 {
-  init_graphics () ;
-  load_database () ;
-  glutMainLoop  () ;
-  return 0 ;
+  init_graphics ();
+  glutMainLoop();
+  return 0;             /* ANSI C requires main to return int. */
 }
-
-
-
