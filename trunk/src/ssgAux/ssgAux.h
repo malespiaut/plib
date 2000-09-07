70b82386fe70224b928b778fd437aed840f7ff62
sjbaker <sjbaker@713b2cfd-b11f-0410-b270-d5401603882f>
2000-09-07 00:56:58 +0000

    Added the ssgaCube class - you gotta start somewhere!

--- trunk/src/ssgAux/ssgAux.h
+++ trunk/src/ssgAux/ssgAux.h
@@ -6,5 +6,16 @@
 #include "ssg.h"
 #include "ssgaShapes.h"
 
+
+#define SSGA_TYPE_SHAPE     0x80000000
+#define SSGA_TYPE_CUBE      0x40000000
+#define SSGA_TYPE_SPHERE    0x20000000
+#define SSGA_TYPE_CYLINDER  0x10000000
+
+inline int ssgaTypeShape   () { return SSGA_TYPE_SHAPE    | ssgTypeBranch () ; }                         
+inline int ssgaTypeCube    () { return SSGA_TYPE_CUBE     | ssgaTypeShape () ; }                         
+inline int ssgaTypeSphere  () { return SSGA_TYPE_SPHERE   | ssgaTypeShape () ; }                         
+inline int ssgaTypeCylinder() { return SSGA_TYPE_CYLINDER | ssgaTypeShape () ; }                         
+
 #endif
 
--- trunk/src/ssgAux/ssgaShapes.h
+++ trunk/src/ssgAux/ssgaShapes.h
@@ -1,2 +1,85 @@
-/* Watch this space! */
+
+#ifndef _SSGASHAPES_H_
+#define _SSGASHAPES_H_  1
+
+
+
+class ssgaShape : public ssgBranch
+{
+  int corrupted ;
+
+protected:
+  virtual void copy_from ( ssgaShape *src, int clone_flags ) ;
+
+  sgVec3 center ;
+  sgVec3 size   ;
+
+  int ntriangles ;
+
+  void init () ;
+
+public:
+  virtual ssgBase *clone ( int clone_flags = 0 ) ;
+  ssgaShape (void) ;
+  ssgaShape ( int numtris ) ;
+  virtual ~ssgaShape (void) ;
+  virtual char *getTypeName(void) ;
+
+  void makeCorrupt () { corrupted = TRUE ; }
+  int  isCorrupt   () { return corrupted ; }
+
+  void setCenter  ( sgVec3 c ) { sgCopyVec3 ( center, c ) ; regenerate () ; }
+  void setSize    ( sgVec3 s ) { sgCopyVec3 ( size  , s ) ; regenerate () ; }
+  void setSize    ( float  s ) { sgSetVec3  ( size,s,s,s) ; regenerate () ; }
+  void setNumTris ( int ntri ) { ntriangles = ntri ; regenerate () ; }
+
+  virtual void regenerate () = 0 ;
+} ;
+
+
+
+class ssgaCube : public ssgaShape
+{
+protected:
+  virtual void copy_from ( ssgaCube *src, int clone_flags ) ;
+public:
+  virtual ssgBase *clone ( int clone_flags = 0 ) ;
+  ssgaCube (void) ;
+  ssgaCube ( int numtris ) ;
+  virtual ~ssgaCube (void) ;
+  virtual char *getTypeName(void) ;
+  virtual void regenerate () ;
+} ;
+
+
+
+class ssgaSphere : public ssgaShape
+{
+protected:
+  virtual void copy_from ( ssgaSphere *src, int clone_flags ) ;
+public:
+  virtual ssgBase *clone ( int clone_flags = 0 ) ;
+  ssgaSphere (void) ;
+  ssgaSphere ( int numtris ) ;
+  virtual ~ssgaSphere (void) ;
+  virtual char *getTypeName(void) ;
+  virtual void regenerate () ;
+} ;
+
+
+
+class ssgaCylinder : public ssgaShape
+{
+protected:
+  virtual void copy_from ( ssgaCylinder *src, int clone_flags ) ;
+public:
+  virtual ssgBase *clone ( int clone_flags = 0 ) ;
+  ssgaCylinder (void) ;
+  ssgaCylinder ( int numtris ) ;
+  virtual ~ssgaCylinder (void) ;
+  virtual char *getTypeName(void) ;
+  virtual void regenerate () ;
+} ;
+
+#endif
 
