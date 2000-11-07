534dae289d1396e2a4cec9ab285b8ab3cea061e2
wolfram_kuss <wolfram_kuss@713b2cfd-b11f-0410-b270-d5401603882f>
2000-11-07 20:30:05 +0000

    .X file loading and saving

---
+++ src/ssg/ssgSaveX.cxx
@@ -0,0 +1,16 @@
+//
+// .X loader for SSG/PLIB
+// .X is the 3D file format for Micro$ofts directX retained mode.
+// Written by Wolfram Kuss (Wolfram.Kuss@t-online.de) in Oct/Nov of 2000
+//
+
+#include "ssgLocal.h"
+
+
+
+int ssgSaveX ( const char *filename, ssgEntity *ent )
+{
+  ulSetError ( UL_WARNING, "ssgSaveX: Not implemented yet" ) ;
+  
+  return FALSE;
+}
