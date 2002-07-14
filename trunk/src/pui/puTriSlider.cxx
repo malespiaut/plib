f498e75251fdf163a582fe251b6ed2bdc82f7d14
puggles <puggles@713b2cfd-b11f-0410-b270-d5401603882f>
2002-07-14 16:01:31 +0000

    John Fay: Fix linux compile warnings, casting.

--- trunk/src/pui/puTriSlider.cxx
+++ trunk/src/pui/puTriSlider.cxx
@@ -126,7 +126,7 @@ void puTriSlider::doHit ( int button, int updown, int x, int y )
 
     next_value = (next_value < 0.0f) ? 0.0f : (next_value > 1.0) ? 1.0f : next_value ;
 
-    int new_value = getMinValue() + (int)( next_value * ( getMaxValue() - getMinValue() ) + 0.5 ) ;
+    float new_value = getMinValue() + next_value * ( getMaxValue() - getMinValue() ) ;
 
     if ( getFreezeEnds() )  // Cannot move end sliders, must move middle one
     {
