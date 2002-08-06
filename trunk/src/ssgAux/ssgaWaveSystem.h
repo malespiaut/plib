7dd48883238cc7c5cbd4f6e19d0c4383d4da3116
sjbaker <sjbaker@713b2cfd-b11f-0410-b270-d5401603882f>
2002-08-06 02:21:06 +0000

    Added multiple wave-trains.

--- trunk/src/ssgAux/ssgaWaveSystem.h
+++ trunk/src/ssgAux/ssgaWaveSystem.h
@@ -3,6 +3,43 @@
 
 typedef float (* ssgaWSDepthCallback ) ( float x, float y ) ;
 
+#define SSGA_MAX_WAVETRAIN  16
+
+class ssgaWaveTrain
+{
+  float height ;
+  float kappa  ;
+  float lambda ;
+  float omega  ;
+  float theta  ;
+
+public:
+
+  ssgaWaveTrain ()
+  {
+    height = 0.5f ;
+    kappa  = 0.8f ;
+    lambda = 1.0f ;
+    omega  = 9.8f * (float) sqrt ( 2.0f/3.0f ) ;
+    theta  = 0.0f ;
+  }
+
+  float getOmega () { return omega  ; }
+  void  setOmega ( float h ) { omega  = h ; }
+
+  float getKappa () { return kappa  ; }
+  void  setKappa ( float h ) { kappa  = h ; }
+
+  float getLambda () { return lambda  ; }
+  void  setLambda ( float h ) { lambda  = h ; }
+
+  float getTheta () { return theta  ; }
+  void  setTheta ( float h ) { theta  = h ; }
+
+  float getWaveHeight () { return height  ; }
+  void  setWaveHeight ( float h ) { height  = h ; }
+} ;
+
 class ssgaWaveSystem : public ssgaShape
 {
   ssgaWSDepthCallback gridGetter ;
@@ -13,12 +50,10 @@ class ssgaWaveSystem : public ssgaShape
   sgVec3 *vertices  ;
   sgVec3 *orig_vertices  ;
 
+  ssgaWaveTrain *train [ SSGA_MAX_WAVETRAIN ] ;
+
   float windSpeed   ;
   float windHeading ;
-  float waveHeight  ;
-  float kappa  ;
-  float lambda ;
-  float omega  ;
   float edgeFlatten ;
 
   float tu, tv ;
@@ -43,15 +78,24 @@ public:
 
   ssgaWSDepthCallback getDepthCallback () { return gridGetter ; } 
 
+  ssgaWaveTrain *getWaveTrain ( int i )
+  {
+    return ( i < 0 || i >= SSGA_MAX_WAVETRAIN ) ? NULL : train [ i ] ;
+  }
+
+  void setWaveTrain ( int i, ssgaWaveTrain *t )
+  {
+    assert ( i >= 0 && i < SSGA_MAX_WAVETRAIN ) ;
+    train [ i ] = t ;
+  }
+
   float getWindSpeed     () { return windSpeed   ; }
   float getWindDirn      () { return windHeading ; }
-  float getWaveHeight    () { return waveHeight  ; }
   float getEdgeFlatten   () { return edgeFlatten ; }
 
   void  setDepthCallback ( ssgaWSDepthCallback cb ) { gridGetter  = cb ; } 
   void  setWindSpeed     ( float speed            ) { windSpeed   = speed ; }
   void  setWindDirn      ( float dirn             ) { windHeading = dirn  ; }
-  void  setWaveHeight    ( float height           ) { waveHeight  = height ; }
   void  setEdgeFlatten   ( float dist             ) { edgeFlatten = dist ; }
   void  setTexScale      ( float u, float v       ) { tu = u ; tv = v ; }
 
