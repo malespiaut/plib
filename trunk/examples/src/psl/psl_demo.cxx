486d0e051e507ac3353914a6b98ba12c3bfad34b
sjbaker <sjbaker@713b2cfd-b11f-0410-b270-d5401603882f>
2002-09-05 06:46:06 +0000

    PSL demo works!

--- trunk/examples/src/psl/psl_demo.cxx
+++ trunk/examples/src/psl/psl_demo.cxx
@@ -6,8 +6,30 @@
 
 /* EMPTY FOR NOW */
 
+float hello ( int argc, float *argv )
+{
+  printf ( "Hello World\n" ) ;
+  return 0.0f ;
+}
+
+PSL_Extension extensions [] =
+{
+  { "hello", 0, hello },
+  { NULL, 0, NULL }
+} ;
+
+
 int main ()
 {
+  PSL_Program *prog = new PSL_Program ( extensions ) ;
+
+  prog -> parse ( "data/test.psl" ) ;
+
+  prog -> dump () ;
+
+  while ( prog -> step () != PSL_PROGRAM_END )
+    /* Do nothing */ ;
+
   exit ( 0 ) ;
 }
 
