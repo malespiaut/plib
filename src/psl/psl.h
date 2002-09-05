fdb8bb508bb6e144faa22c335b8d13efa9ad8631
sjbaker <sjbaker@713b2cfd-b11f-0410-b270-d5401603882f>
2002-09-05 07:09:13 +0000

    PSL demo now runs two scripts in parallel.

--- src/psl/psl.h
+++ src/psl/psl.h
@@ -12,6 +12,7 @@ enum PSL_Result
 typedef unsigned char PSL_Opcode ;
 class PSL_Context ;
 class PSL_Parser  ;
+class PSL_Program ;
 
 
 class PSL_Extension
@@ -19,21 +20,35 @@ class PSL_Extension
 public:
   char *symbol ;
   int   argc ;
-  float (*func) ( int, float * ) ;
+  float (*func) ( int, float *, PSL_Program *p ) ;
 } ;
 
 
 
 class PSL_Program
 {
-  PSL_Opcode  *code    ;
-  PSL_Context *context ;
-  PSL_Parser  *parser  ;
+  PSL_Opcode     *code       ;
+  PSL_Context    *context    ;
+  PSL_Parser     *parser     ;
+  PSL_Extension  *extensions ;
+
+  void *userData ;
 
 public:
 
    PSL_Program ( PSL_Extension *ext ) ;
+   PSL_Program ( PSL_Program *src ) ;
+
   ~PSL_Program () ;
+
+  PSL_Context   *getContext     () { return context    ; }
+  PSL_Opcode    *getCode        () { return code       ; }
+  PSL_Parser    *getParser      () { return parser     ; }
+  PSL_Extension *getExtensions  () { return extensions ; }
+
+  void      *getUserData ()           { return userData ; }
+  void       setUserData ( void *ud ) { userData = ud ; }
+
   void       dump  () ;
   int        parse ( char *fname ) ;
   int        parse ( FILE *fd    ) ;
