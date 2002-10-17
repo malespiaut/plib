74a769bdfcd91246d08eef8b57521f09cd01e6e1
sjbaker <sjbaker@713b2cfd-b11f-0410-b270-d5401603882f>
2002-10-17 13:42:31 +0000

    Updated FLT writer to generate texture and vertex palette entries.

--- trunk/src/ssg/ssgSaveFLT.cxx
+++ trunk/src/ssg/ssgSaveFLT.cxx
@@ -34,7 +34,67 @@
 #include <stdio.h>
 #include "ssgLocal.h"
 
-static FILE* save_fd;
+
+struct FLT_texture
+{
+  char fname [ 200 ] ;   /* 200 is the OpenFlight limit - don't increase it! */
+} ;
+
+
+struct FLT_vertex
+{
+  sgVec3 vert ;
+  sgVec3 norm ;
+  sgVec4 col  ;
+  sgVec2 tex  ;
+} ;
+
+
+static ulList *texList = NULL ;
+static ulList *vtxList = NULL ;
+static FILE   *save_fd = NULL ;
+
+static int addToTexturePool ( char *tex )
+{
+  int max =  texList -> getNumEntities () ;
+
+  for ( int i = 0 ; i < max ; i++ )
+  {
+    FLT_texture *target = (FLT_texture *)( texList -> getEntity ( i ) ) ;
+
+    if ( strcmp ( target->fname, tex ) == 0 )
+      return i ;
+  }
+
+  FLT_texture *add = new FLT_texture ;
+
+  strncpy ( add->fname, tex, 199 ) ;
+
+  texList -> addEntity ( add ) ;
+  return max ;
+}
+
+
+static int addToVertexPool ( FLT_vertex *vert )
+{
+  int max =  vtxList -> getNumEntities () ;
+
+  for ( int i = 0 ; i < max ; i++ )
+  {
+    FLT_vertex *target = (FLT_vertex *)( vtxList -> getEntity ( i ) ) ;
+
+    if ( memcmp ( target, vert, sizeof ( FLT_vertex ) ) == 0 )
+      return i ;
+  }
+
+  FLT_vertex *add = new FLT_vertex ;
+
+  *add = *vert ;
+
+  vtxList -> addEntity ( add ) ;
+  return max ;
+}
+
 
 static int writeChar ( char val )
 {
@@ -61,7 +121,7 @@ static int writeInt( int val )
 }
 
 
-void swabInt ( int *x )
+static void swabInt ( int *x )
 {
   unsigned int t = (unsigned int) *x ;
 
@@ -72,7 +132,15 @@ void swabInt ( int *x )
 }
 
 
-void swabDouble ( double *x )
+static void swabFloat ( float *x )
+{
+  int *a = (int *) x ;
+
+  swabInt ( a ) ;
+}
+
+
+static void swabDouble ( double *x )
 {
   int *a = (int *) x ;
   int *b = a+1 ;
@@ -85,6 +153,16 @@ void swabDouble ( double *x )
   *a = *a ^ *b ;
 }
 
+
+
+static int writeFloat ( float val )
+{
+  if ( ulIsLittleEndian )
+    swabFloat ( & val ) ;
+
+  fwrite ( & val, sizeof(float), 1, save_fd ) ;
+  return sizeof(float) ;
+}
 
 
 static int writeDouble( double val )
@@ -188,10 +266,10 @@ static void writeHeader ()
 }
 
 
-void writePush () { writeShort ( 10 ) ; writeShort (  4 ) ; }
-void writePop  () { writeShort ( 11 ) ; writeShort (  4 ) ; }
+static void writePush () { writeShort ( 10 ) ; writeShort (  4 ) ; }
+static void writePop  () { writeShort ( 11 ) ; writeShort (  4 ) ; }
 
-void writeObject ()
+static void writeObject ()
 {
   writeShort ( 4 ) ;
   writeShort ( 2+2+8+4+2+2+2+2+2+2 ) ;
@@ -207,31 +285,39 @@ void writeObject ()
 
 
 
-void writeFace ()
+static void writeFace ( ssgLeaf *leaf )
 {
   writeShort ( 5 ) ;
   writeShort ( 2+2+8+4+2+1+1+2+2+1+1+2+2+2+2+2+4+2+1+1+
                4+1+1+2+4+4+4+2+2+4+4+2+2 ) ;
   writeString ( "1234567", 8 ) ;  /* Name */
-  writeInt   ( 0 ) ;     /* IR color code */
-  writeShort ( 0 ) ;     /* Relative Priority */
-  writeChar  ( 0 ) ;     /* Draw Type */
+  writeInt   ( 0 ) ;    /* IR color code */
+  writeShort ( 0 ) ;    /* Relative Priority */
+  writeChar  ( 0 ) ;    /* Draw Type */
   writeChar  ( 0 ) ;    /* Texture-White */
-  writeShort ( 0 ) ;/* XXXX */ /* Color name index */
+  writeShort ( 0 ) ;    /* Color name index */
   writeShort ( 0 ) ;    /* Alternate Color name index */
   writeChar  ( 0 ) ;
   writeChar  ( 0 ) ;    /* Billboard */
   writeShort (-1 ) ;    /* Detail Texture */
-  writeShort (-1 ) ;/* XXXX */ /* Texture */
+
+  ssgSimpleState *s = (ssgSimpleState *)( leaf -> getState () ) ;
+
+  if ( s != NULL && s -> getTextureFilename () != NULL )
+    writeShort ( addToTexturePool ( s -> getTextureFilename () ) ) ;
+  else
+    writeShort (-1 ) ; /* No Texture */
+
   writeShort (-1 ) ;    /* Material */
   writeShort ( 0 ) ;/* XXXX */ /* SMC */
   writeShort ( 0 ) ;    /* DFAD FID */
   writeInt   ( 0 ) ;    /* IR Material Code */
-  writeShort ( 0 ) ;    /* Transparenct */
+  writeShort ( 0 ) ;    /* Transparent */
   writeChar  ( 0 ) ;    /* LOD generation control */
   writeChar  ( 0 ) ;    /* Line Style */
-  writeInt   ( 1<<3 ) ; /* Flags == Packed Colour */
-  writeChar  ( 0 ) ;    /* Light mode */
+  /* SJB: I think this means "get colour from the vertex" */
+  writeInt   ( 1<<1 ) ; /* Flags == No Colour */
+  writeChar  ( 3 ) ;    /* Light mode == Use Vertex colour & Normal */
   writeChar  ( 0 ) ;
   writeShort ( 0 ) ;
   writeInt   ( 0 ) ;
@@ -245,23 +331,35 @@ void writeFace ()
   writeShort ( 0 ) ;
 }
 
-void writeLeaf ( ssgLeaf *leaf )
+static void writeLeaf ( ssgLeaf *leaf )
 {
   writeObject () ;
   writePush   () ;
-  writePop    () ;
 
   for ( int i = 0 ; i < leaf -> getNumTriangles () ; i++ )
   {
-    writeFace () ;
-    writePush () ;
+    writeFace ( leaf ) ;
 
-    writePop  () ;
+    for ( int i = 0 ; i < leaf -> getNumVertices () ; i++ )
+    {
+      writeShort ( 72 ) ;  /* Vertex List */
+      writeShort ( 2+2+4 ) ;
+
+      FLT_vertex vert ;
+      sgCopyVec3 ( vert.vert, leaf -> getVertex   ( i ) ) ;
+      sgCopyVec3 ( vert.norm, leaf -> getNormal   ( i ) ) ;
+      sgCopyVec4 ( vert.col , leaf -> getColour   ( i ) ) ;
+      sgCopyVec2 ( vert.tex , leaf -> getTexCoord ( i ) ) ;
+
+      writeInt ( addToVertexPool ( & vert ) ) ;
+    }
   }
+
+  writePop    () ;
 }
 
 
-void writeGroup ( ssgBranch *bra )
+static void writeGroup ( ssgBranch *bra )
 {
   writeShort ( 2 ) ;
   writeShort ( 2+2+8+2+2+4+2+2+2+1+1+4 ) ;
@@ -278,7 +376,7 @@ void writeGroup ( ssgBranch *bra )
 }
 
 
-void writeEntity ( ssgEntity *ent )
+static void writeEntity ( ssgEntity *ent )
 {
   if ( ent == NULL )
     return ;
@@ -301,10 +399,88 @@ void writeEntity ( ssgEntity *ent )
 }
 
 
+static void collectPalettesFromLeaf ( ssgLeaf *leaf ) 
+{
+  ssgSimpleState *s = (ssgSimpleState *)( leaf -> getState () ) ;
+
+  if ( s != NULL && s -> getTextureFilename () != NULL )
+    texList -> addEntity ( s -> getTextureFilename () ) ;
+
+  for ( int i = 0 ; i < leaf -> getNumVertices () ; i++ )
+  {
+    FLT_vertex vert ;
+    sgCopyVec3 ( vert.vert, leaf -> getVertex   ( i ) ) ;
+    sgCopyVec3 ( vert.norm, leaf -> getNormal   ( i ) ) ;
+    sgCopyVec4 ( vert.col , leaf -> getColour   ( i ) ) ;
+    sgCopyVec2 ( vert.tex , leaf -> getTexCoord ( i ) ) ;
+    addToVertexPool ( & vert ) ;
+  }
+}
+
+static void collectPalettes ( ssgEntity *ent )
+{
+  if ( ent == NULL )
+    return ;
+
+  if ( ! ent -> isAKindOf ( ssgTypeBranch () ) )
+  {
+    collectPalettesFromLeaf ( (ssgLeaf *)ent ) ;
+    return ;
+  }  
+
+  for ( int i = 0 ; i < ((ssgBranch *)ent) -> getNumKids() ; i++ )
+    collectPalettes ( ((ssgBranch *)ent) -> getKid ( i ) ) ;
+}
+
+static void writePalettes ( ssgEntity *root )
+{
+  int i ;
+
+  collectPalettes ( root ) ;
+
+  for ( i = 0 ; i < texList -> getNumEntities () ; i++ )
+  {
+    writeShort  ( 64 ) ;  /* Texture Palette */
+    writeShort  ( 2+2+200+4+4+4 ) ;
+    writeString ( ((FLT_texture *)(texList -> getEntity ( i )))->fname, 200 ) ;
+    writeInt    ( i ) ;
+    writeInt    ( i % 16 ) ;
+    writeInt    ( i / 16 ) ;
+  }
+
+  writeShort  ( 67 ) ;  /* Vertex Palette header. */
+  writeShort  ( 2+2+4 ) ;
+  writeShort  ( 2+2+4 + 
+                ( 2+2+2+2+8+8+8+4*3+4*2+4+4 ) * vtxList -> getNumEntities () ) ;
+
+  for ( i = 0 ; i < vtxList -> getNumEntities () ; i++ )
+  {
+    FLT_vertex *vv = (FLT_vertex *)(vtxList -> getEntity ( i )) ;
+
+    writeShort  ( 70 ) ;  /* Vertex Palette with Normal and Texture */
+    writeShort  ( 2+2+2+2+8+8+8+4*3+4*2+4+4 ) ;
+    writeShort  ( 0 ) ;  /* Colour name index */
+    writeShort  ( 1<<3 ) ;  /* Flags == Use Packed Colour */
+    writeDouble ( vv -> vert [ 0 ] ) ;
+    writeDouble ( vv -> vert [ 1 ] ) ;
+    writeDouble ( vv -> vert [ 2 ] ) ;
+    writeFloat  ( vv -> norm [ 0 ] ) ;
+    writeFloat  ( vv -> norm [ 1 ] ) ;
+    writeFloat  ( vv -> norm [ 2 ] ) ;
+    writeFloat  ( vv -> tex [ 0 ] ) ;
+    writeFloat  ( vv -> tex [ 1 ] ) ;
+    writeInt    ( ( (int) ( vv -> col [ 3 ] * 255.0f ) << 24 ) +
+                  ( (int) ( vv -> col [ 2 ] * 255.0f ) << 16 ) +
+                  ( (int) ( vv -> col [ 1 ] * 255.0f ) <<  8 ) +
+                  ( (int) ( vv -> col [ 0 ] * 255.0f ) <<  0 ) ) ;
+    writeInt    ( 0 ) ;
+  }
+}
+
+
+
 int ssgSaveFLT ( const char *fname, ssgEntity *root )
 {
-  save_fd = NULL ;
-
   if ( ( save_fd = fopen ( fname, "wb") ) == NULL )
   {
     fprintf ( stderr, "Could not open file '%s' for writing.\n", fname ) ;
@@ -313,9 +489,30 @@ int ssgSaveFLT ( const char *fname, ssgEntity *root )
 
   writeHeader () ;
 
-  writeEntity ( root ) ;
+  if ( texList == NULL )  texList = new ulList (  20 ) ;
+  if ( vtxList == NULL )  vtxList = new ulList ( 200 ) ;
+
+  writePalettes ( root ) ;
+  writeEntity   ( root ) ;
 
   fclose ( save_fd ) ;
+
+  /* Cleanup */
+
+  int max, i ;
+
+  max = texList -> getNumEntities () ;
+
+  for ( i = 0 ; i < max ; i++ )
+    delete (FLT_texture *) texList -> getEntity ( i ) ; 
+  delete texList ;
+
+  max = vtxList -> getNumEntities () ;
+
+  for ( i = 0 ; i < max ; i++ )
+    delete (FLT_vertex *) vtxList -> getEntity ( i ) ; 
+  delete vtxList ;
+
   return TRUE ;
 }
 
