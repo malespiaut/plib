deb38cb6bb01058235ea035c2cd196fcbb9892f4
wolfram_kuss <wolfram_kuss@713b2cfd-b11f-0410-b270-d5401603882f>
2004-03-24 17:19:02 +0000

    Fixing compiler warning.

--- trunk/examples/src/pw/pw_pui.cxx
+++ trunk/examples/src/pw/pw_pui.cxx
@@ -63,7 +63,7 @@ int main ( int, char ** )
   pwSetCallbacks ( kbFunc, msFunc, mpFunc, rsFunc, exFunc ) ;
   while ( 1 )
   {
-    glClearColor ( 0.5, 0.1, 0.1, 1.0 ) ;
+    glClearColor ( 0.5f, 0.1f, 0.1f, 1.0f ) ;
     glClear ( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT ) ;
     puDisplay () ;
     pwSwapBuffers () ;
