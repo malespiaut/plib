3f774db7eebb94eb9a084e9c4993a6e438ca07bb
fayjf <fayjf@713b2cfd-b11f-0410-b270-d5401603882f>
2010-02-27 03:44:00 +0000

    Implementing a Windows 7 joystick patch sent from Frederic Bouvier1/26/10 (see e-mail from John Fay to PLIB_devel 2/27/10 around 3:40 AM GMT)

--- trunk/src/js/jsWindows.cxx
+++ trunk/src/js/jsWindows.cxx
@@ -63,7 +63,7 @@ bool os_specific_s::getOEMProductName ( jsJoystick* joy, char *buf, int buf_sz )
 
   if ( lr != ERROR_SUCCESS )
   {
-    // XP/Vista seem to have moved it to "current user"
+    // XP/Vista/7 seem to have moved it to "current user"
     lr = RegOpenKeyEx ( HKEY_CURRENT_USER, key, 0, KEY_QUERY_VALUE, &hKey) ;
     if ( lr != ERROR_SUCCESS ) return false ;
   }
@@ -84,7 +84,12 @@ bool os_specific_s::getOEMProductName ( jsJoystick* joy, char *buf, int buf_sz )
 
   lr = RegOpenKeyEx ( HKEY_LOCAL_MACHINE, key, 0, KEY_QUERY_VALUE, &hKey ) ;
 
-  if ( lr != ERROR_SUCCESS ) return false ;
+  if ( lr != ERROR_SUCCESS )
+  {
+    // XP/Vista/7 seem to have moved it to "current user"
+    lr = RegOpenKeyEx ( HKEY_CURRENT_USER, key, 0, KEY_QUERY_VALUE, &hKey) ;
+    if ( lr != ERROR_SUCCESS ) return false ;
+  }
 
   // Get OEM Name
   dwcb = buf_sz ;
